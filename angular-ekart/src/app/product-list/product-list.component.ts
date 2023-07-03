e2b8d92dacbf2ec60462ec353ea891e820b82ee1
YassFreeks <y.moutaouakil@hotmail.com>
2023-07-03 10:49:54 +0200

    event binding

--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  name="John Doe";
+  // name="John Smith";
+  addToCart:number = 0;
   product= {
     name: "Iphone X",
     price: 789,
@@ -19,6 +20,23 @@ export class ProductListComponent {
 
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+
+  onNameChange(event: any){
+    // this.name = event.target.value;
+    // console.log(event.target.value);
+  }
+
+  decrementCartValue() {
+    if(this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if(this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
   }
 
   
