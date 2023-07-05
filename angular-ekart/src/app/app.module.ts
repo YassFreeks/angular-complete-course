8abe8c2dc96d7b72cf10e5613a544aa1d8abec97
YassFreeks <y.moutaouakil@hotmail.com>
2023-07-05 12:52:27 +0200

    two way data binding

--- angular-ekart/src/app/app.module.ts
+++ angular-ekart/src/app/app.module.ts
@@ -6,16 +6,17 @@ import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { SearchComponent } from './product-list/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    TestComponent, HeaderComponent, NavbarComponent, ProductListComponent
+    TestComponent, HeaderComponent, NavbarComponent, ProductListComponent, SearchComponent
   ],
   imports : [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers : [],
   bootstrap: [AppComponent]
